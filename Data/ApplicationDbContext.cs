033d589902fc5cf04b57c4ef80ab9d1793b865ad
vysnoo <vishnukumaresan21@gmail.com>
2024-09-16 13:54:38 +0530

    Database Migration Completed

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CRUD_Application.Models;
+using Microsoft.EntityFrameworkCore;
+namespace CRUD_Application.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {   
+        }
+        public DbSet<Name> Names { get; set; }
+    }
+}
---
+++ Models/Name.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_Application.Models
+{
+    public class Name {
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public String? FirstName { get; set; }
+        [Required]
+        public String? LastName { get; set; }
+    }
+}
