221a0807db3d98c163aba51bf0781730c07124ad
vysnoo <vishnukumaresan21@gmail.com>
2024-09-17 21:47:44 +0530

    Create Operation Done

--- Controllers/CrudController.cs
+++ Controllers/CrudController.cs
@@ -16,5 +16,31 @@ namespace CRUD_Application.Controllers
             IEnumerable<Name> objList = _db.Names.ToList();
             return View(objList);
         }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //GET
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Name obj)
+        {
+            if(obj.FirstName==obj.LastName)
+            {
+                ModelState.AddModelError("LastName","First Name & Last Name does not exact same");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Names.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
     }
 }
