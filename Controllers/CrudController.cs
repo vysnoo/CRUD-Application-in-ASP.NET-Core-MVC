2802706cfe93279767bfc66a780ec4017f82e1bd
vysnoo <vishnukumaresan21@gmail.com>
2024-09-18 09:40:41 +0530

    Update and Delete Operation Done

--- Controllers/CrudController.cs
+++ Controllers/CrudController.cs
@@ -23,7 +23,7 @@ namespace CRUD_Application.Controllers
             return View();
         }
 
-        //GET
+        //POST
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Name obj)
@@ -40,6 +40,78 @@ namespace CRUD_Application.Controllers
                 return RedirectToAction("Index");
             }
             return View(obj);
+        }
+
+        //GET
+        public IActionResult Update(int? id)
+        {
+            if(id==0 || id == null)
+            {
+                return NotFound();
+            }
+
+            var objList = _db.Names.Find(id);
+
+            if (objList == null)
+            {
+                return NotFound();
+            }
+
+            return View(objList);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Name obj)
+        {
+            if (obj.FirstName == obj.LastName)
+            {
+                ModelState.AddModelError("LastName", "First Name & Last Name does not exact same");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Names.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+
+            var objList = _db.Names.Find(id);
+
+            if (objList == null)
+            {
+                return NotFound();
+            }
+
+            return View(objList);
+        }
+
+        //GET
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var objList= _db.Names.Find(id);
+            if (objList == null)
+            {
+                return NotFound();
+            }
+
+                _db.Names.Remove(objList);
+                _db.SaveChanges();
+                return RedirectToAction("Index");   
+         
         }
 
     }
